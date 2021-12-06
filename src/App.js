9861be09bb3e42674d945411005ac331ab147ea5
ThiagoAndres <andreswcc11@gmail.com>
2021-12-06 12:09:48 -0500

    Busca de productos desde input text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { Fragment, useState } from 'react';
 import './App.css';
+import Producto from './Producto';
 
-function App() {
+//JSX
+export default function App() {
+  const productos = [
+    {
+      "nombre": "Raspberry Pi 3",
+      "desc": "Kit Raspberry Pi 3. Incluye cargador, memoria",
+      "precio": "$ 50"
+    },
+    {
+      "nombre": "Televisor 50 pulgadas",
+      "desc": "Televisor smart tv",
+      "precio": "$ 1000"
+    },
+    {
+      "nombre": "Celular Xiaomi",
+      "desc": "Cámara 50MP. 128GB memoria.",
+      "precio": "$ 700"
+    },
+    {
+      "nombre": "Celular Samsung",
+      "desc": "Cámara 70MP. 250GB memoria.",
+      "precio": "$ 950"
+    }
+  ];
+  //Estado: lugar donde se guarda datos (variable) y tiene una función para modificar esos datos
+
+  const [listaProductos, setListaProductos] = useState(productos);
+
+  const buscaProducto = (evento) => {
+    setListaProductos(productos.filter(pro => pro.nombre.toLowerCase().includes(evento.target.value.toLowerCase())))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Fragment>
+      <input type="text" placeholder="Buscar producto..." onChange={buscaProducto} />
+
+      {listaProductos.map(pro =>
+        <Producto
+          nombre={pro.nombre}
+          desc={pro.desc}
+          precio={pro.precio}
+        />)}
+    </Fragment>
+  )
 }
 
-export default App;
